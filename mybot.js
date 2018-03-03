e97ebce9dfc49217824af366769d200d65c00943
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2018-03-03 17:33:11 +0200

    added new features
    added kick ban purge

--- mybot.js
+++ mybot.js
@@ -6,10 +6,10 @@ const fs = require("fs")
 
 client.on("ready", () => {
     console.log("Running");
-    client.user.setActivity('&help')
+    client.user.setActivity('&help | better than Teafox')
 });
 
-client.on("message", (message) => {
+client.on("message", async message => {
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
     //command getter
@@ -33,6 +33,47 @@ client.on("message", (message) => {
                 message.channel.send(text);
             }
             message.delete();
+        } else
+        if (command === "kick") {
+            if (!message.member.roles.some(r => ["White Gangsta"].includes(r.name)))
+                return message.reply("Sorry, you don't have permissions to use this!");
+            let member = message.mentions.members.first();
+            if (!member)
+                return message.reply("Please mention a valid member of this server");
+            if (!member.kickable)
+                return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+            let reason = args.slice(1).join(' ');
+            if (!reason)
+                return message.reply("Please indicate a reason for the kick!");
+            await member.kick(reason)
+                .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+            message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+        } else
+        if (command === "ban") {
+            if (!message.member.roles.some(r => ["White Gangsta"].includes(r.name)))
+                return message.reply("Sorry, you don't have permissions to use this!");
+            let member = message.mentions.members.first();
+            if (!member)
+                return message.reply("Please mention a valid member of this server");
+            if (!member.bannable)
+                return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+            let reason = args.slice(1).join(' ');
+            if (!reason)
+                return message.reply("Please indicate a reason for the ban!");
+            await member.ban(reason)
+                .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+            message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+        } else
+        if (command === "purge") {
+            const deleteCount = parseInt(args[0], 10);
+            if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+                return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+            const fetched = await message.channel.fetchMessages({
+                count: deleteCount
+            });
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+            message.reply("Deleted " + deleteCount + " messages")
         }
     }
 
